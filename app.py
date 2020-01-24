2ccb7e796096be24dc26686af254b5da98b845a5
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-01-24 17:19:17 +0100

    probando volley

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/hasfallen/', methods=['GET', 'POST'])
+@app.route('/hasfallen/', methods=['GET'])
 def has_fallen():
     accel = request.args.get('accel')
 
