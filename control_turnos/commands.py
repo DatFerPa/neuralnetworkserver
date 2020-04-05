4d87cf3e0680bbb2509760f352da1111eccbc8b3
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-04-05 23:32:07 +0200

    primera prueba con insercion de datos

--- control_turnos/commands.py
+++ control_turnos/commands.py
@@ -18,5 +18,16 @@ def drop_tables():
 @click.command(name='generate_data')
 @with_appcontext
 def generate_data():
-    #insertar datos para la bbdd
-    a=1
+    turno1 = Turno(nombre_t="turno dia", maquina="mc 102023")
+    turno2 = Turno(nombre_t="turno noche", maquina="23-405-5")
+    turno3 = Turno(nombre_t="turno de explotacion",mquina="455-345-567")
+    maq1 = Maquinista(nombre_m="Fer")
+    maq2 = Maquinista(nombre_m="Fernando")
+    maq1.turnos.append(turno1)
+    maq1.turnos.append(turno2)
+    maq1.turnos.append(turno3)
+    maq2.turnos.append(turno1)
+    maq2.turnos.append(turno2)
+    db.session.add(maq1)
+    db.session.add(maq2)
+    db.session.commit()
