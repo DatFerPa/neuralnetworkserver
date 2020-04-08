74b46f3d9f53033fd45dc10cb73bf0649e5c4c77
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-04-09 00:26:54 +0200

    chnage

--- control_turnos/routes.py
+++ control_turnos/routes.py
@@ -3,8 +3,11 @@ from flask import BLueprints, render_template
 from .extensions import db
 from .models import User, Turno
 
-main = BLueprint('main',__name)
+main = BLueprint('main',__name__)
 
 @main.route('/')
 def principal():
     return render_template('principal.html')
+
+@main.route('')
+def login():
