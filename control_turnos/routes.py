8a1a8bbdebf4d92af3778d7006864ed3067bea05
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-04-10 03:46:47 +0200

    movidita del servidor

--- control_turnos/routes.py
+++ control_turnos/routes.py
@@ -12,3 +12,29 @@ def principal():
 @main.route('/login/',methods=['POST'])
 def login():
     nombre = request.args.get('nombre')
+    maquinista = Maquinista.query.filter_by(nombre_m=nombre).first()
+    if maquinista is None:
+        return 'no'
+    else:
+        return 'si'
+
+@main.route('/turnos/',methods=['POST'])
+def login():
+    nombre = request.args.get('nombre')
+    maquinista = Maquinista.query.filter_by(nombre_m=nombre).first()
+    turnos_union = turnos.query.filter_by(maquinista_id=maquinista.id).all()
+    turnos_de_un_maquinista = []
+
+    for turn in turnos_union:
+        turno_actual = Turno.query.filter_by(id=turn.turno_id).first()
+        turnos_de_un_maquinista.append(turno_actual)
+
+    concatenacion_turnos=""
+    iteracion = len(turnos_de_un_maquinista) - 1
+
+    for n in range(iteracion):
+        concatenacion_turnos = concatenacion_turnos + turnos_de_un_maquinista[n].nombre_t + ";" + turnos_de_un_maquinista[n].maquina
+        if n != iteracion:
+            concatenacion_turnos = concatenacion_turnos + ":"
+
+    return concatenacion_turnos
