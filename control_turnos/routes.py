bdde5126729f4b52935e4016c769afd8c117d3f9
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-04-27 05:57:31 +0200

    redirect

--- control_turnos/routes.py
+++ control_turnos/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 
 from .extensions import db
 from .models import Maquinista, Turno, turnos
