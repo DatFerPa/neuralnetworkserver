c5fbdd4ee1bc2cd2109c7bb4ac08053973267934
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-05-25 21:09:21 +0200

    update deleting

--- control_turnos/routes.py
+++ control_turnos/routes.py
@@ -138,6 +138,21 @@ def quitarMaquinista():
     else:
         db.session.delete(maquinista)
         db.session.commit()
+
+        STORAGE_ACCOUNT_NAME = 'ficherosmaquinistas'
+        STORAGE_ACCOUNT_KEY  = 'JKGDYu80C4HWg6DxUyA8mWYouPVAHV9tlB8MO6Xcv5sFKR7KVr+Onw7PLwP7KjMqhdPKTCWFk59NM4m+t/lcGQ=='
+        account = CloudStorageAccount(STORAGE_ACCOUNT_NAME,STORAGE_ACCOUNT_KEY)
+        file_service = account.create_file_service()
+        files = list(file_service.list_directories_and_files('shareficherosmaquinistas',prefix=maquinista.nombre_m))
+        for file in files:
+            print(file.name)
+            file_service.delete_file(
+                "shareficherosmaquinistas",
+                None,
+                file.name
+            )
+            print("--------------------")
+
         return redirect(url_for('main.quitMaquinistas',ok_maquinista=True))
 
 
