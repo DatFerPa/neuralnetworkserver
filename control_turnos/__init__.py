d88a35fba156589e52505e93e6e26a69f94b921b
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-06-05 06:19:09 +0200

    aaaaaaaaa

--- control_turnos/__init__.py
+++ control_turnos/__init__.py
@@ -9,7 +9,7 @@ from .models import Administrador
 def create_app(config_file='settings.py'):
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = SECRET_KEY
+    app.config['SECRET_KEY'] = 'secret_key_que_jamas_se_adivinará$¿?'
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config['SESSION_PERMANENT']= False
 
