a5ab18a9ec6bcefa2ac86a5947a030a165284e65
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-06-05 05:08:35 +0200

    adminsitrador another time

--- control_turnos/models.py
+++ control_turnos/models.py
@@ -6,7 +6,7 @@ db.Column('maquinista_id',db.Integer,db.ForeignKey('maquinista.id'),primary_key=
 db.Column('turno_id',db.Integer,db.ForeignKey('turno.id'),primary_key=True)
 )
 
-class Maquinista(UserMixin, db.Model):
+class Maquinista(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     nombre_m = db.Column(db.String(80), unique=True, nullable=False)
     turnos = db.relationship('Turno',secondary=turnos,lazy='subquery',
@@ -17,7 +17,7 @@ class Turno(db.Model):
     nombre_t = db.Column(db.String(50),unique=True,nullable=False)
     maquina = db.Column(db.String(50),unique=True,nullable=False)
 
-class Administrador(db.Model):
+class Administrador(UserMixin, db.Model):
     id = db.Column(db.Integer,primary_key=True)
     nombre_admin = db.Column(db.String(50),unique=True,nullable=False)
     password_admin = db.Column(db.String(50),nullable=False)
