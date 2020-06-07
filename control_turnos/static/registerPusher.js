66920d2fd6e76101a52fec52053e7041288ae2a2
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-06-07 20:41:50 +0200

    and this one?

--- control_turnos/static/registerPusher.js
+++ control_turnos/static/registerPusher.js
@@ -9,7 +9,7 @@ var pusher = new Pusher('33da5fe2d909596436d5', {
     //data.turno
      //alert(JSON.stringify(data));
      let identificador = "alert" + Date.now();
-     $(".container").prepend("<div class=\"alert alert-secondary alert-dismissible fade show\" role=\"alert\"> <h4 class=\"alert-heading\"> Un maquinista ha tenido un accidente </h4> El maquinista <a href=\" {{ url_for('webRoutes.logsTurno',"+data.nombreFichero+","+data.turno+","+data.fecha+") }} \" class=\"alert-link\">"+ data.maquinista +" ha sufrido un accidente en el turno: "+ data.turno+"</a>  <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button></div>");
+     $(".container").prepend("<div class=\"alert alert-secondary alert-dismissible fade show\" role=\"alert\"> <h4 class=\"alert-heading\"> Un maquinista ha tenido un accidente </h4> El maquinista <a href=\" /logsTurno/?nombre_fichero_arg="+data.nombreFichero+"&fecha_fichero_arg="+data.fecha+"&nombre_turno_arg="+data.turno+" \" class=\"alert-link\">"+ data.maquinista +" ha sufrido un accidente en el turno: "+ data.turno+"</a>  <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button></div>");
 
 
    });
