7397747e1b69a6076e74c53e92f73456ca31c0ee
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-06-07 08:32:04 +0200

    a ver si con los alert mejor

--- control_turnos/static/registerPusher.js
+++ control_turnos/static/registerPusher.js
@@ -8,7 +8,8 @@ var pusher = new Pusher('33da5fe2d909596436d5', {
     //data.maquinista
     //data.turno
      //alert(JSON.stringify(data));
-     let identificador = "toast" + Date.now();
-     $("#toastContainer").append("<div id=\" "+ identificador +" \" class=\"toast\" data-autohide=\"false\" role=\"alert\" aria-live=\"assertive\" aria-atomic=\"true\"><div class=\"toast-header\"><strong class=\"mr-auto\">Alarma de hombre muerto</strong><small class=\"text-muted\">just now</small><button type=\"button\" class=\"ml-2 mb-1 close\" data-dismiss=\"toast\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button></div><div class=\"toast-body\">See? Just like this.</  div></div>");
-     $("#"+identificador).toast('show');
+     let identificador = "alert" + Date.now();
+     $(".container").append("<div class=\"alert alert-secondary alert-dismissible fade show\" role=\"alert\"> Un maquinista ha tenido un accidente   <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button></div>");
+
+
    });
