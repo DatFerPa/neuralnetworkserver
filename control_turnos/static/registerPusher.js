19ee52f8c343063a0d0f2ef69ad072bb3032af7f
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-06-07 08:04:35 +0200

    jijijojo

--- control_turnos/static/registerPusher.js
+++ control_turnos/static/registerPusher.js
@@ -10,5 +10,5 @@ var pusher = new Pusher('33da5fe2d909596436d5', {
      //alert(JSON.stringify(data));
      let identificador = "toast" + Date.now();
      $("#toastContainer").append("<div id=\" "+ identificador +" \" class=\"toast\" data-autohide=\"false\" role=\"alert\" aria-live=\"assertive\" aria-atomic=\"true\"><div class=\"toast-header\"><strong class=\"mr-auto\">Alarma de hombre muerto</strong><small class=\"text-muted\">just now</small><button type=\"button\" class=\"ml-2 mb-1 close\" data-dismiss=\"toast\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button></div><div class=\"toast-body\">See? Just like this.</  div></div>");
-     $("#"+identificador).toast('show')
+     $("#"+identificador).toast('show');
    });
