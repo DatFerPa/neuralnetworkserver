5ba3bff1857cb170790b11158087c14f7a302415
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-06-30 05:09:18 +0200

    contexto

--- control_turnos/tests/__init__.py
+++ control_turnos/tests/__init__.py
@@ -7,11 +7,12 @@ from control_turnos.models import Maquinista, Turno, Administrador
 class BaseTestClass(unittest.TestCase):
 
     def setUp(self):
-        print("inicio test")
         self.app = create_app(config_file="settings_testing.py")
         self.client = self.app.test_client()
-
+        with self.app.app_context():
+            db.create_all()
 
 
     def tearDown(self):
-        print("Fin test")
+        with self.app.app_context():
+            db.session.remove()
