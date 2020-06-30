b8b1f9bc8954158911f42964b65ee3cecea1049d
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-06-30 06:31:49 +0200

    intuicion

--- control_turnos/tests/test_pruebas_admin.py
+++ control_turnos/tests/test_pruebas_admin.py
@@ -7,11 +7,11 @@ class PruebasAdmin(BaseTestClass):
     def test_haciendo_login(self):
         res = self.client.post('/loginAdmin/',data=dict(nombreAdmin="admin",passwordAdmin="admin"),follow_redirects=True)
         self.assertIn(b'Panel de control',res.data)
+        self.client.get('/logoutAdmin/')
 
     def test_entrando_sin_login(self):
         res = self.client.get('/addTurnos/')
         self.assertIn(b'Iniciar sesion de administrador',res.data)
-
 
 
 
