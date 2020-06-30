7a4a6bcde4f80d3cc9b86c59acd5f024cb5c555a
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-06-30 06:48:47 +0200

    turnos add maquinista

--- control_turnos/tests/test_pruebas_admin.py
+++ control_turnos/tests/test_pruebas_admin.py
@@ -11,17 +11,28 @@ class PruebasAdmin(BaseTestClass):
 
     def test_entrando_sin_login(self):
         res = self.client.get('/addTurnos/')
-        self.assertIn(b'Iniciar sesion de administrador',res.data)
+        self.assertIn(b'Redirecting',res.data)
 
+    def test_add_maquinista(self):
+        self.client.post('/loginAdmin/',data=dict(nombreAdmin="admin",passwordAdmin="admin"),follow_redirects=True)
+        res = self.client.post('/nuevoMaquinista/',data=dict(nombre="Maquinista1"),follow_redirects=True)
+        self.assertIn(b'se ha añadido',res.data)
+        with self.app.app_context():
+            maquinista = Maquinista.query.filter_by(nombre_m="Maquinista1").first()
+            self.assertEqual(maquinista.nombre_m,"Maquinista1")
+            db.session.delete(maquinista)
+            db.session.command()
+            maquinista = Maquinista.query.filter_by(nombre_m="Maquinista1").first()
+            self.assertEqual(maquinista,None)
+        self.client.get('/logoutAdmin/')
 
-
-
-
-
-
-
-
-
+    def test_add_maquinista_repetido(self):
+        self.client.post('/loginAdmin/',data=dict(nombreAdmin="admin",passwordAdmin="admin"),follow_redirects=True)
+        res = self.client.post('/nuevoMaquinista/',data=dict(nombre="Maquinista1"),follow_redirects=True)
+        self.assertIn(b'se ha añadido',res.data)
+        res = self.client.post('/nuevoMaquinista/',data=dict(nombre="Maquinista1"),follow_redirects=True)
+        self.assertIn(b'ya se encuentra',res.data)
+        self.client.get('/logoutAdmin/')
 
 
 
