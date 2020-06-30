c96a6fb54472af4ab2f09d6aa8c0c88f3cdc5e5d
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-06-30 06:23:00 +0200

    los bytes

--- control_turnos/tests/test_pruebas_admin.py
+++ control_turnos/tests/test_pruebas_admin.py
@@ -6,11 +6,11 @@ class PruebasAdmin(BaseTestClass):
 
     def test_haciendo_login(self):
         res = self.client.post('/loginAdmin/',data=dict(nombreAdmin="admin",passwordAdmin="admin"),follow_redirects=True)
-        self.assertIn('Panel de control',res.data)
+        self.assertIn(b'Panel de control',res.data)
 
     def test_entrando_sin_login(self):
         res = self.client.get('/addTurnos/')
-        self.assertIn("Iniciar sesion de administrador",res.data)
+        self.assertIn(b'Iniciar sesion de administrador',res.data)
 
 
 
