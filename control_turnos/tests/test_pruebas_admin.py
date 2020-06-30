1da02b65ccbedbfd7931647051f1361b633a0101
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-06-30 07:20:02 +0200

    mas pruebas

--- control_turnos/tests/test_pruebas_admin.py
+++ control_turnos/tests/test_pruebas_admin.py
@@ -21,7 +21,7 @@ class PruebasAdmin(BaseTestClass):
             maquinista = Maquinista.query.filter_by(nombre_m="Maquinista1").first()
             self.assertEqual(maquinista.nombre_m,"Maquinista1")
             db.session.delete(maquinista)
-            db.session.command()
+            db.session.commit()
             maquinista = Maquinista.query.filter_by(nombre_m="Maquinista1").first()
             self.assertEqual(maquinista,None)
         self.client.get('/logoutAdmin/')
@@ -29,10 +29,113 @@ class PruebasAdmin(BaseTestClass):
     def test_add_maquinista_repetido(self):
         self.client.post('/loginAdmin/',data=dict(nombreAdmin="admin",passwordAdmin="admin"),follow_redirects=True)
         res = self.client.post('/nuevoMaquinista/',data=dict(nombre="Maquinista1"),follow_redirects=True)
-        self.assertIn(b'se ha añadido',res.data)
+        self.assertIn(b'se ha',res.data)
         res = self.client.post('/nuevoMaquinista/',data=dict(nombre="Maquinista1"),follow_redirects=True)
         self.assertIn(b'ya se encuentra',res.data)
         self.client.get('/logoutAdmin/')
+        with self.app.app_context():
+            maquinista = Maquinista.query.filter_by(nombre_m="Maquinista1").first()
+            db.session.delete(maquinista)
+            db.session.commit()
+
+
+    def test_add_turno(self):
+        self.client.post('/loginAdmin/',data=dict(nombreAdmin="admin",passwordAdmin="admin"),follow_redirects=True)
+        res = self.client.post('/nuevoTurno/',data=dict(nombreTurno="Turno1",nombreMaquina="Maquina1"),follow_redirects=True)
+        self.assertIn(b'se ha',res.data)
+        res = self.client.post('/nuevoTurno/',data=dict(nombreTurno="Turno2",nombreMaquina="Maquina1"),follow_redirects=True)
+        with self.app.app_context():
+            turno = Turno.query.filter_by(nombre_t="Turno1",maquina="Maquina1").first()
+            turno2 = Turno.query.filter_by(nombre_t="Turno2",maquina="Maquina1").first()
+            self.assertEqual(turno.nombre_t,"Turno1")
+            self.assertEqual(turno.maquina,"Maquina1")
+            self.assertEqual(turno.nombre_t,"Turno2")
+            self.assertEqual(turno.maquina,"Maquina1")
+            db.session.delete(turno)
+            db.session.delete(turno2)
+            db.session.commit()
+            turno = Turno.query.filter_by(nombre_t="Turno1",maquina="Maquina1").first()
+            turno2 = Turno.query.filter_by(nombre_t="Turno2",maquina="Maquina1").first()
+            self.assertEqual(turno,None)
+
+        self.client.get('/logoutAdmin/')
+
+
+    def test_add_turno_repetido(self):
+        self.client.post('/loginAdmin/',data=dict(nombreAdmin="admin",passwordAdmin="admin"),follow_redirects=True)
+        res = self.client.post('/nuevoTurno/',data=dict(nombreTurno="Turno1",nombreMaquina="Maquina1"),follow_redirects=True)
+        self.assertIn(b'se ha',res.data)
+        res = self.client.post('/nuevoTurno/',data=dict(nombreTurno="Turno1",nombreMaquina="Maquina1"),follow_redirects=True)
+        self.assertIn(b'ya se encuentra',res.data)
+        self.client.get('/logoutAdmin/')
+        with self.app.app_context():
+            turno = Turno.query.filter_by(nombre_t="Turno1",maquina="Maquina1").first()
+            db.session.delete(turno)
+            db.session.commit()
+
+        self.client.get('/logoutAdmin/')
+
+
+    def test_quit_maquinista(self):
+        with self.app.app_context():
+            maquinista = Maquinista(nombre_m="Maquinista1")
+            db.session.add(maquinsta_prueba)
+            db.session.commit()
+        self.client.post('/loginAdmin/',data=dict(nombreAdmin="admin",passwordAdmin="admin"),follow_redirects=True)
+        res = self.client.post('/quitarMaquinista/',data=dict(nombre="Maquinista1"),follow_redirects=True)
+        self.assertIn(b'se ha eliminado',res.data)
+        with self.app.app_context():
+            maquinista = Maquinista.query.filter_by(nombre_m="Maquinista1").first()
+            self.assertEqual(maquinista,None)
+        self.client.get('/logoutAdmin/')
+
+    def test_quit_maquinista_noExiste(self):
+        with self.app.app_context():
+            maquinista = Maquinista.query.filter_by(nombre_m="Maquinista1").first()
+            self.assertEqual(maquinista,None)
+        self.client.post('/loginAdmin/',data=dict(nombreAdmin="admin",passwordAdmin="admin"),follow_redirects=True)
+        res = self.client.post('/quitarMaquinista/',data=dict(nombre="Maquinista1"),follow_redirects=True)
+        self.assertIn(b'no se encuentra',res.data)
+        with self.app.app_context():
+            maquinista = Maquinista.query.filter_by(nombre_m="Maquinista1").first()
+            self.assertEqual(maquinista,None)
+        self.client.get('/logoutAdmin/')
+
+
+
+    def test_quit_turno(self):
+        with self.app.app_context():
+            turno = Turno(nombre_t="Turno1",maquina="Maquina1")
+            db.session.add(turno_prueba)
+            db.session.commit()
+        self.client.post('/loginAdmin/',data=dict(nombreAdmin="admin",passwordAdmin="admin"),follow_redirects=True)
+        res = self.client.post('/quitarTurno/',data=dict(nombreTurno="Turno1"),follow_redirects=True)
+        self.assertIn(b'se ha eliminado',res.data)
+        with self.app.app_context():
+            turno = Turno.query.filter_by(nombre_t="Turno1").first()
+            self.assertEqual(turno,None)
+        self.client.get('/logoutAdmin/')
+
+    def test_quit_turno_noExiste(self):
+        with self.app.app_context():
+            turno = Turno.query.filter_by(nombre_t="Turno1").first()
+            self.assertEqual(turno,None)
+        self.client.post('/loginAdmin/',data=dict(nombreAdmin="admin",passwordAdmin="admin"),follow_redirects=True)
+        res = self.client.post('/quitarTurno/',data=dict(nombreTurno="Turno1"),follow_redirects=True)
+        self.assertIn(b'no se encuentra',res.data)
+        with self.app.app_context():
+            turno = Turno.query.filter_by(nombre_t="Turno1").first()
+            self.assertEqual(turno,None)
+        self.client.get('/logoutAdmin/')
+
+
+
+
+
+
+
+
+
 
 
 
