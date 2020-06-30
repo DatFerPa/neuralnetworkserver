a3178ec28e105081a862cf0127a18ca51f70e43c
Fernando Palazuelo <fpalazuelo@gmail.com>
2020-06-30 05:44:57 +0200

    fuera

--- control_turnos/tests/__init__.py
+++ control_turnos/tests/__init__.py
@@ -9,11 +9,3 @@ class BaseTestClass(unittest.TestCase):
     def setUp(self):
         self.app = create_app(config_file="settings_testing.py")
         self.client = self.app.test_client()
-        with self.app.app_context():
-            db.create_all()
-
-
-    def tearDown(self):
-        with self.app.app_context():
-            db.session.remove()
-            db.drop_all()
